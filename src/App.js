9c0fb80dcbada25eca03dd529122c881945269a0
rjmcnamara10 <rjmcnamara10@gmail.com>
2024-03-02 22:41:18 -0500

    Added header with logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Ticket Track</title>
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      </Helmet>
       <header className="App-header">
-        <h1>Welcome to My AWS Amplify React App</h1>
-        <p>This is an example text displayed using AWS Amplify UI React.</p>
+        <img src="/logo192.png" alt="Logo" />
+        <h1>Ticket Track</h1>
       </header>
+      <div className="Main-content">
+        <p>This is an example text displayed using AWS Amplify UI React.</p>
+      </div>
     </div>
   );
 }
