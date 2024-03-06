5e5d91fb983b0c92c05576081ce0be5d569b6e7b
rjmcnamara10 <rjmcnamara10@gmail.com>
2024-03-05 23:05:57 -0500

    Continuing work on ticket display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { Card, ScrollView } from '@aws-amplify/ui-react';
+import { ScrollView } from '@aws-amplify/ui-react';
 import './App.css';
 
 function App() {
@@ -21,21 +21,29 @@ function App() {
       </Helmet>
       <header className="App-header">
         <img src="/logo192.png" alt="Logo" className="logo" />
-        <img src="/title.png" alt="Logo" className="title" />
+        <img src="/title.png" alt="Title" className="title" />
       </header>
       <div className="Main-content">
         <ScrollView width="1000px" maxWidth="100%">
-          <ul className="Card-container">
+          <div className="ticket-container">
             {tickets.map(ticket => (
-              <Card key={ticket.id}> {/* Wrap each ticket in a Card */}
-                <p>Section: {ticket.section}</p>
-                <p>Row: {ticket.row}</p>
-                <p>Price: ${ticket.price}</p>
-                <p>App: {ticket.app}</p>
-                <p>Location Points: {ticket.locationPoints}</p>
-              </Card>
+              <div className={`ticket-tile ${ticket.app}`}>
+                <div className="seat-view"></div>
+                <div className="ticket-info">
+                  <span className="app">
+                    {ticket.app}
+                  </span>
+                  <span className="seat">
+                    Section {ticket.section}<br />
+                    Row {ticket.row}
+                  </span>
+                  <span className="price">
+                    ${ticket.price}
+                  </span>
+                </div>
+              </div>
             ))}
-          </ul>
+          </div>
         </ScrollView>
       </div>
     </div>
