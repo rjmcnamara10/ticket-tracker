1eef70606bb46595409aaff72124ba5b180a4289
rjmcnamara10 <rjmcnamara10@gmail.com>
2024-03-06 23:04:29 -0500

    Side panel and ticket app logos

--- src/App.js
+++ src/App.js
@@ -23,28 +23,31 @@ function App() {
         <img src="/logo192.png" alt="Logo" className="logo" />
         <img src="/title.png" alt="Title" className="title" />
       </header>
-      <div className="Main-content">
-        <ScrollView width="1000px" maxWidth="100%">
-          <div className="ticket-container">
-            {tickets.map(ticket => (
-              <div className={`ticket-tile ${ticket.app}`}>
-                <div className="seat-view"></div>
-                <div className="ticket-info">
-                  <span className="app">
-                    {ticket.app}
-                  </span>
-                  <span className="seat">
-                    Section {ticket.section}<br />
-                    Row {ticket.row}
-                  </span>
-                  <span className="price">
-                    ${ticket.price}
-                  </span>
+      <div className="App-content-container">
+        <div className="Side-panel"></div>
+        <div className="Main-content">
+          <ScrollView width="800px" maxWidth="100%" height="275px">
+            <div className="ticket-container">
+              {tickets.map(ticket => (
+                <div className={`ticket-tile ${ticket.app}`}>
+                  <div className="seat-view"></div>
+                  <div className="ticket-info">
+                    <div className="app">
+                      <img src={`/ticket_app_logos/${ticket.app}_logo.png`} alt={`${ticket.app} logo`} />
+                    </div>
+                    <span className="seat">
+                      Section {ticket.section}<br />
+                      Row {ticket.row}
+                    </span>
+                    <span className="price">
+                      ${ticket.price}
+                    </span>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </ScrollView>
+              ))}
+            </div>
+          </ScrollView>
+        </div>
       </div>
     </div>
   );
