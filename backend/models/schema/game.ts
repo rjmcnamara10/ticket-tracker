5390145db7a4f8ed0ca12507f07401fd436f8953
rjmcnamara10 <rjmcnamara10@gmail.com>
2024-12-13 21:56:30 -0500

    Adding jsdoc comments to schema

--- backend/models/schema/game.ts
+++ backend/models/schema/game.ts
@@ -1,5 +1,17 @@
 import { Schema } from 'mongoose';
 
+/**
+ * Mongoose schema for the Game collection. Defines the structure for storing games in the database.
+ *
+ * - homeTeam: The full name of the home team.
+ * - awayTeam: The full name of the away team.
+ * - startDateTime: The date and time when the game starts in EST.
+ * - venue: The name of the venue where the game is played.
+ * - city: The city where the game is played.
+ * - state: The two-letter abbreviation of the state where the game is played.
+ * - tickets: The tickets available for the game.
+ * - lastUpdated: The date and time when the tickets available was last updated. Optional field.
+ */
 const gameSchema: Schema = new Schema(
   {
     homeTeam: {
@@ -10,6 +22,7 @@ const gameSchema: Schema = new Schema(
     },
     startDateTime: {
       type: Date,
+      unique: true,
     },
     venue: {
       type: String,
--- backend/models/schema/ticket.ts
+++ backend/models/schema/ticket.ts
@@ -1,5 +1,15 @@
 import { Schema } from 'mongoose';
 
+/**
+ * Mongoose schema for the Ticket collection. Defines the structure for storing tickets in the database.
+ *
+ * - section: The section number of the arena where the ticket is located.
+ * - row: The row number of the section where the ticket is located.
+ * - price: The price of the ticket in USD.
+ * - quantity: The number of tickets available to purchase at the price and location.
+ * - app: The ticket resale app where the ticket is listed.
+ * - link: The link to the ticket listing.
+ */
 const ticketSchema: Schema = new Schema(
   {
     section: {
