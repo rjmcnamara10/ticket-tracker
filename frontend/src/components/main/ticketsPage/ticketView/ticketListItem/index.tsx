5781c92436247d4d752281e1a700848affda7f57
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-05-12 17:41:19 -0400

    ticket list view formatting enhancements

--- frontend/src/components/main/ticketsPage/ticketView/ticketListItem/index.tsx
+++ frontend/src/components/main/ticketsPage/ticketView/ticketListItem/index.tsx
@@ -1,5 +1,6 @@
 import { Ticket } from '../../../../../types';
 import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 /**
  * Represents a list item component for displaying the information associated with a ticket.
@@ -11,31 +12,29 @@ const TicketListItem = ({ ticket }: { ticket: Ticket }) => {
 
   return (
     <a href={ticket.link} className='list-item ticket-list-item'>
-      {/* <div className='seat-view-list'>
-        <img
-          src={`/balcony-views/${ticket.section}.webp`}
-          alt={`View from Section ${ticket.section}`}
-        />
-      </div> */}
       <div className='ticket-app-list'>
         <img
           src={`/ticket_app_logos/${lowercaseAppName}_logo.png`}
           alt={`${lowercaseAppName} logo`}
         />
       </div>
-      {/* <div className='ticket-info-list'>
-        <span className='seat-list'>
-          Section {ticket.section}
-          <br />
-          Row {ticket.row}
-        </span>
-        <span className='price-list'>${ticket.price}</span>
-      </div> */}
       <span className='seat-list'>
-        Section {ticket.section}
-        <br />
-        Row {ticket.row}
+        Section {ticket.section} - Row {ticket.row}
       </span>
+      <div
+        className='seat-view-list'
+        onClick={e => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}>
+        <i className='fas fa-binoculars'></i>
+        <div className='seat-view-image-list'>
+          <img
+            src={`/balcony-views/${ticket.section}.webp`}
+            alt={`View from Section ${ticket.section}`}
+          />
+        </div>
+      </div>
       <span className='price-list'>${ticket.price}</span>
     </a>
   );
