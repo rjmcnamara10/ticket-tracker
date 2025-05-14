741a15da6b77cdbc3e1e30eb09688423d695c941
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-05-14 16:06:55 -0400

    Click on app logo navigates to home page

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,13 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import './index.css';
 
 /**
  * Represents the header component for the application, containing the logo and title.
  */
-const Header = () => (
-  <header className='header'>
-    <img src={'/logo_header.png'} alt='Logo' className='logo' />
-    <img src={'/title.png'} alt='Title' className='title' />
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+
+  /**
+   * Function to handle navigation to the home page on logo click.
+   */
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <header className='header'>
+      <img src={'/logo_header.png'} alt='Logo' className='logo' onClick={handleLogoClick} />
+      <img src={'/title.png'} alt='Title' className='title' />
+    </header>
+  );
+};
 
 export default Header;
