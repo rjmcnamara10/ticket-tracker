0e3dc4fce1df7f5277c9c4a59138f2a9822e1459
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-05-14 15:25:55 -0400

    Updating header images and sizing

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -5,7 +5,7 @@ import './index.css';
  */
 const Header = () => (
   <header className='header'>
-    <img src={'/logo192.png'} alt='Logo' className='logo' />
+    <img src={'/logo_header.png'} alt='Logo' className='logo' />
     <img src={'/title.png'} alt='Title' className='title' />
   </header>
 );
